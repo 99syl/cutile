b3f2366a68154dcdacba185a3b4e4d378579d86e
BaguetteFramboise <120696021+BaguetteFramboise@users.noreply.github.com>
2023-11-03 15:31:04 +0100

    Network API: socket bind and listen + minor refactors

--- network.h
+++ network.h
@@ -57,24 +57,56 @@ enum net_error
     net_win32_wsa_permission_denied,
     net_win32_wsa_drop_conn_on_network_reset,
     net_win32_wsa_socket_not_connected,
+    net_win32_wsa_address_already_in_use,
+    net_win32_wsa_invalid_address,
+    net_win32_wsa_socket_already_connected,
+    net_win32_wsa_unsupported_operation,
 #endif // _WIN32
 
     net_unknown_err
 };
 
-struct net_socket_options
+net_error  open_net_socket(net_socket_kind kind, net_address_family family, net_protocol protocol, net_socket* out);
+net_error  close_net_socket(net_socket socket);
+
+struct net_endpoint
 {
-    const char*         hostname;
+    net_address_family  family;
+    u8                  data[32];
+};
+struct ipv4_endpoint
+{
+    net_address_family  family;
     u16                 port;
-    net_socket_kind     kind;
-    net_address_family  address_family;
-    net_protocol        protocol;
+    u8                  bytes[4];
+    u8                  zero[26];
+};
+struct ipv6_endpoint
+{
+    net_address_family  family;
+    u16                 port;
+    union
+    {
+        u8              bytes[16];
+        u16             words[8];        
+    };
+    u8                  zero[14];
 };
 #ifdef CUTILE_C
-typedef struct net_socket_options net_socket_options;
+typedef struct net_endpoint net_endpoint;
+typedef struct ipv4_endpoint ipv4_endpoint;
+typedef struct ipv6_endpoint ipv6_endpoint;
 #endif // CUTILE_C
-net_error  open_net_socket(net_socket_options* options, net_socket* out);
-net_error  close_net_socket(net_socket socket);
+bool8           is_ipv4_cstr_valid(const char* ip);
+ipv4_endpoint   create_ipv4_endpoint(const char* ip, u16 port);
+
+net_error  bind_net_socket(net_socket socket, const net_endpoint* endpoint);
+#ifdef CUTILE_CPP
+template <typename EndpointType>
+net_error  bind_net_socket(net_socket socket, const EndpointType* endpoint);
+#endif // CUTILE_CPP
+
+net_error  net_socket_listen(net_socket socket, u32 backlog);
 
 net_error  send_to_net_socket(net_socket dest_socket, u8* data, u64 len);
 
@@ -97,29 +129,33 @@ net_error unload_platform_net_library();
 
 #ifdef _WIN32
 #include <winsock2.h>
+#include <Ws2tcpip.h>
+#ifdef _MSC_VER
+#pragma comment(lib, "Ws2_32.lib")
+#endif // _MSC_VER
 #endif // _WIN32
 
-net_error open_net_socket(net_socket_options* options, net_socket* out)
+net_error open_net_socket(net_socket_kind kind, net_address_family family, net_protocol protocol, net_socket* out)
 {
     int af = 0;
     int type = 0;
-    int protocol = 0;
+    int proto = 0;
 
 #ifdef _WIN32
-    switch (options->address_family)
+    switch (family)
     {
-    case net_af_ipv4: af = AF_INET;
-    case net_af_ipv6: af = AF_INET6;
+    case net_af_ipv4: af = AF_INET; break;
+    case net_af_ipv6: af = AF_INET6; break;
     }
-    switch (options->kind)
+    switch (kind)
     {
-    case net_socket_stream: type = SOCK_STREAM;
-    case net_socket_datagram: type = SOCK_DGRAM;
+    case net_socket_stream: type = SOCK_STREAM; break;
+    case net_socket_datagram: type = SOCK_DGRAM; break;
     }
-    switch (options->protocol)
+    switch (protocol)
     {
-    case net_tcp_protocol: protocol = IPPROTO_TCP;
-    case net_udp_protocol: protocol = IPPROTO_UDP;
+    case net_tcp_protocol: proto = IPPROTO_TCP; break;
+    case net_udp_protocol: proto = IPPROTO_UDP; break;
     }
 #endif
 
@@ -170,16 +206,183 @@ net_error close_net_socket(net_socket socket)
     }
 #endif // _WIN32
     return net_no_error;
+
+}
+
+bool8 is_ipv4_cstr_valid(const char* ip)
+{
+    u32 byte = 0;
+    u8  sep_count = 0;
+    for (u32 i = 0; ip[i]; ++i)
+    {
+        char c = ip[i];
+        if (c == '.')
+        {
+            ++sep_count;
+            if (sep_count > 3) return bool8_false;
+            if (byte > 255) return bool8_false;
+            byte = 0;
+        }
+        else if (c >= '0' && c <= '9')
+        {
+            byte *= 10;
+            byte += c - '0';
+        }
+        else return bool8_false;
+    }
+    if (byte > 255) return bool8_false;
+    if (sep_count != 3) return bool8_false;
+    return bool8_true;
+}
+ipv4_endpoint create_ipv4_endpoint(const char* ip, u16 port)
+{
+    ipv4_endpoint endp;
+    endp.family = net_af_ipv4;
+    u8 byte = 0;
+    u32 byte_index = 0;
+    for (u32 i = 0; ip[i]; ++i)
+    {
+        char c = ip[i];
+        if (c >= '0' && c <= '9')
+        {
+            byte *= 10;
+            byte += c - '0';
+        }
+        else if (c == '.')
+        {
+            endp.bytes[byte_index++] = byte;
+            byte = 0;
+        }
+    }
+    endp.bytes[byte_index] = byte;
+    endp.port = port;
+    return endp;
+}
+
+net_error bind_net_socket(net_socket socket, const net_endpoint* endpoint)
+{
+    int res;
+    switch (endpoint->family)
+    {
+    case net_af_ipv4:
+    {
+        struct sockaddr_in addr;
+        ipv4_endpoint* v4_ep = (ipv4_endpoint*)endpoint;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(v4_ep->port);
+#ifdef _WIN32
+        addr.sin_addr.S_un.S_un_b.s_b1 = v4_ep->bytes[0];
+        addr.sin_addr.S_un.S_un_b.s_b2 = v4_ep->bytes[1];
+        addr.sin_addr.S_un.S_un_b.s_b3 = v4_ep->bytes[2];
+        addr.sin_addr.S_un.S_un_b.s_b4 = v4_ep->bytes[3];
+#endif
+        res = bind(socket.handle, (struct sockaddr*)&addr, sizeof(addr));
+        break;
+    }
+    case net_af_ipv6:
+    {
+        struct sockaddr_in6 addr;
+        ipv6_endpoint* v6_ep = (ipv6_endpoint*)endpoint;
+        addr.sin6_family = AF_INET6;
+        addr.sin6_scope_id = 0;
+        addr.sin6_port = htons(v6_ep->port);
+#ifdef _WIN32
+        for (u32 i = 0; i < 16; ++i) addr.sin6_addr.u.Byte[i] = v6_ep->bytes[i];
+#endif
+        res = bind(socket.handle, (struct sockaddr*)&addr, sizeof(addr));
+        break;
+    }
+    }
+
+#ifdef _WIN32
+    if (res == SOCKET_ERROR)
+    {
+        int err = WSAGetLastError();
+        switch (err)
+        {
+        case WSANOTINITIALISED:
+            return net_win32_wsa_not_initialized;
+        case WSAENETDOWN:
+            return net_win32_wsa_network_down;
+        case WSAEACCES:
+            return net_win32_wsa_permission_denied;
+        case WSAEADDRINUSE:
+            return net_win32_wsa_address_already_in_use;
+        case WSAEADDRNOTAVAIL:
+            return net_win32_wsa_invalid_address;
+        case WSAEFAULT:
+            return net_win32_wsa_invalid_data_address;
+        case WSAEINPROGRESS:
+            return net_win32_wsa_busy;
+        case WSAEINVAL:
+            return net_win32_wsa_invalid_argument;
+        case WSAENOBUFS:
+            return net_win32_wsa_insufficient_buffer_space;
+        case WSAENOTSOCK:
+            return net_win32_wsa_not_a_sock;
+        default:
+            return net_unknown_err;
+        }
+    }
+#endif // _WIN32
+    return net_no_error;
+} // bind_net_socket
+#ifdef CUTILE_CPP
+template <typename EndpointType>
+net_error bind_net_socket(net_socket socket, const EndpointType* endpoint)
+{
+    return bind_net_socket(socket, (net_endpoint*)endpoint);
+}
+template net_error bind_net_socket<ipv4_endpoint>(net_socket, const ipv4_endpoint*);
+template net_error bind_net_socket<ipv6_endpoint>(net_socket, const ipv6_endpoint*);
+#endif // CUTILE_CPP
+
+net_error net_socket_listen(net_socket socket, u32 backlog)
+{
+    int res = listen(socket.handle, backlog);
+
+#ifdef _WIN32
+    if (res == SOCKET_ERROR)
+    {
+        int err = WSAGetLastError();
+        switch (err)
+        {
+        case WSANOTINITIALISED:
+            return net_win32_wsa_not_initialized;
+        case WSAENETDOWN:
+            return net_win32_wsa_network_down;
+        case WSAEADDRINUSE:
+            return net_win32_wsa_address_already_in_use;
+        case WSAEINPROGRESS:
+            return net_win32_wsa_busy;
+        case WSAEINVAL:
+            return net_win32_wsa_invalid_argument;
+        case WSAEISCONN:
+            return net_win32_wsa_socket_already_connected;
+        case WSAEMFILE:
+            return net_win32_wsa_too_much_opened_sockets;
+        case WSAENOBUFS:
+            return net_win32_wsa_insufficient_buffer_space;
+        case WSAENOTSOCK:
+            return net_win32_wsa_not_a_sock;
+        case WSAEOPNOTSUPP:
+            return net_win32_wsa_unsupported_operation;
+        default:
+            return net_unknown_err;
+        }
+    }
+#endif
+    return net_no_error;
 }
 
 net_error send_to_net_socket(net_socket dest_socket, u8* data, u64 len)
 {
-#ifdef _WIN32
     u32 sent;
     u32 total = 0;
     while (total < len)
     {
         sent = send(dest_socket.handle, (char*)data, len, 0);
+#ifdef _WIN32
         if (sent == SOCKET_ERROR)
         {
             int err = WSAGetLastError();
@@ -200,9 +403,9 @@ net_error send_to_net_socket(net_socket dest_socket, u8* data, u64 len)
             default: return net_unknown_err;
             }
         }
+#endif // _WIN32
         total += sent;
     }
-#endif
 
     return net_no_error;
 }
@@ -271,6 +474,10 @@ const char* get_net_error_msg(net_error err)
     case net_win32_wsa_permission_denied: return "Permission denied. An attempt was made to access a socket in a way forbidden by its access permissions. An example is using a broadcast address for sendto without broadcast permission being set using setsockopt(SO_BROADCAST). Another possible reason for the WSAEACCES error is that when the bind function is called (on Windows NT 4.0 with SP4 and later), another application, service, or kernel mode driver is bound to the same address with exclusive access. Such exclusive access is a new feature of Windows NT 4.0 with SP4 and later, and is implemented by using the SO_EXCLUSIVEADDRUSE option.";
     case net_win32_wsa_drop_conn_on_network_reset: return "Network dropped connection on reset. The connection has been broken due to keep-alive activity detecting a failure while the operation was in progress. It can also be returned by setsockopt if an attempt is made to set SO_KEEPALIVE on a connection that has already failed.";
     case net_win32_wsa_socket_not_connected: return "Socket is not connected. A request to send or receive data was disallowed because the socket is not connected and (when sending on a datagram socket using sendto) no address was supplied. Any other type of operation might also return this error—for example, setsockopt setting SO_KEEPALIVE if the connection has been reset.";
+    case net_win32_wsa_address_already_in_use: return "Address already in use. Typically, only one usage of each socket address (protocol/IP address/port) is permitted. This error occurs if an application attempts to bind a socket to an IP address/port that has already been used for an existing socket, or a socket that was not closed properly, or one that is still in the process of closing. For server applications that need to bind multiple sockets to the same port number, consider using setsockopt (SO_REUSEADDR). Client applications usually need not call bind at all—connect chooses an unused port automatically. When bind is called with a wildcard address (involving ADDR_ANY), a WSAEADDRINUSE error could be delayed until the specific address is committed. This could happen with a call to another function later, including connect, listen, WSAConnect, or WSAJoinLeaf.";
+    case net_win32_wsa_invalid_address: return "Cannot assign requested address. The requested address is not valid in its context. This normally results from an attempt to bind to an address that is not valid for the local computer. This can also result from connect, sendto, WSAConnect, WSAJoinLeaf, or WSASendTo when the remote address or port is not valid for a remote computer (for example, address or port 0).";
+    case net_win32_wsa_socket_already_connected: return "Socket is already connected. A connect request was made on an already-connected socket. Some implementations also return this error if sendto is called on a connected SOCK_DGRAM socket (for SOCK_STREAM sockets, the to parameter in sendto is ignored) although other implementations treat this as a legal occurrence.";
+    case net_win32_wsa_unsupported_operation: return "Operation not supported. The attempted operation is not supported for the type of object referenced. Usually this occurs when a socket descriptor to a socket that cannot support this operation is trying to accept a connection on a datagram socket.";
 #endif // _WIN32
         
     case net_unknown_err: return "An unknown network error occurred.";
