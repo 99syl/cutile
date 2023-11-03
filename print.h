8c9b292a4ff641bdf4e21e1ae14a2089e2456419
BaguetteFramboise <120696021+BaguetteFramboise@users.noreply.github.com>
2023-11-03 10:45:13 +0100

    C++: Add print function with variadic arguments and fmt

--- print.h
+++ print.h
@@ -1,18 +1,30 @@
 #ifndef CUTILE_PRINT_H
 #define CUTILE_PRINT_H
 
-#include "./cutile.h"
+#include "./str.h"
 
-struct string;
-#ifdef CUTILE_C
-typedef struct string string;
-#endif
-
-void print(const char* cstr);
-void print(string* str);
-
-void println(const char* cstr);
-void println(string* str);
+void print(const string* str);
+void print_cstr(const char* cstr);
+void println(const string* str);
+void println_cstr(const char* cstr);
+#ifdef CUTILE_CPP
+template <typename ...Args>
+inline void print(allocator* allocator, const char* fmt, Args ...args);
+template <typename ...Args>
+inline void print(const char* fmt, Args ...args);
+template <typename ...Args>
+inline void print(allocator* allocator, const string* fmt, Args ...args);
+template <typename ...Args>
+inline void print(const string* fmt, Args ...args);
+template <typename ...Args> 
+inline void println(allocator* allocator, const char* fmt, Args ...args);
+template <typename ...Args> 
+inline void println(const char* fmt, Args ...args);
+template <typename ...Args>
+inline void println(allocator* allocator, const string* fmt, Args ...args);
+template <typename ...Args>
+inline void println(const string* fmt, Args ...args);
+#endif // CUTILE_CPP
 
 #ifdef CUTILE_IMPLEM
 
@@ -21,22 +33,8 @@ void println(string* str);
 #elif defined(__unix__)
 #include <unistd.h>
 #endif
-#include "./str.h"
 
-void print(const char* cstr)
-{
-#ifdef _WIN32
-    auto stdout = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (stdout && stdout != INVALID_HANDLE_VALUE) 
-    {
-        DWORD written;
-        WriteConsoleA(stdout, cstr, cstr_length(cstr), &written, NULL);
-    }
-#elif defined(__unix__)
-    write(1, cstr, cstr_length(cstr));
-#endif
-}
-void print(string* str)
+void print(const string* str)
 {
 #ifdef _WIN32
     auto stdout = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -49,18 +47,66 @@ void print(string* str)
     write(1, str->data, str->count);
 #endif
 }
-
-void println(const char* cstr)
+void print_cstr(const char* cstr)
 {
-    print(cstr);
-    print("\n");
+#ifdef _WIN32
+    auto stdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (stdout && stdout != INVALID_HANDLE_VALUE) 
+    {
+        DWORD written;
+        WriteConsoleA(stdout, cstr, cstr_length(cstr), &written, NULL);
+    }
+#elif defined(__unix__)
+    write(1, cstr, cstr_length(cstr));
+#endif
 }
-void println(string* str)
+void println(const string* str)
 {
     print(str);
     print("\n");
 }
+void println_cstr(const char* cstr)
+{
+    print(cstr);
+    print("\n");
+}
 
-#endif
+#endif // CUTILE_IMPLEM
 
-#endif
+// Inline implementations:
+#ifdef CUTILE_CPP
+template <typename ...Args>
+inline void print(allocator* allocator, const char* fmt, Args ...args)
+{
+    string str = format_str(allocator, fmt, args...);
+    print(&str);
+    deallocate(allocator, &str);
+}
+template <typename ...Args>
+inline void print(const char* fmt, Args ...args)
+{
+    print(&basic_heap_allocator, fmt, args...);
+}
+template <typename ...Args>
+inline void print(allocator* allocator, const string* fmt, Args ...args)
+{
+    string str = str_format(allocator, fmt, args...);
+    print(&str);
+    deallocate(allocator, &str);
+}
+template <typename ...Args>
+inline void print(const string* fmt, Args ...args)
+{
+    print(&basic_heap_allocator, fmt, args...);
+}
+template <typename ...Args> 
+inline void println(allocator* allocator, const char* fmt, Args ...args) { print(allocator, fmt, args..., '\n'); }
+template <typename ...Args> 
+inline void println(const char* fmt, Args ...args) { println(&basic_heap_allocator, fmt, args...); }
+template <typename ...Args>
+inline void println(allocator* allocator, const string* fmt, Args ...args) { print(allocator, fmt, args..., '\n'); }
+template <typename ...Args>
+inline void println(const string* fmt, Args ...args) { println(&basic_heap_allocator, fmt, args...); }
+#endif // CUTILE_CPP
+
+#endif // !CUTILE_PRINT_H
