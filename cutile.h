347f36ab0ed91320729dc20f237657effb67fc48
Syl <99syl.dev@proton.me>
2023-11-05 13:01:40 +0100

    Require at least C11

--- cutile.h
+++ cutile.h
@@ -3,8 +3,14 @@
 
 #ifdef __cplusplus
 #define CUTILE_CPP
-#else
+#else // !__cplusplus
 #define CUTILE_C
 #endif
+
+#ifdef CUTILE_C
+#if __STDC_VERSION__ < 201112L
+#error "Only C11 and next versions are supported by cutile."
+#endif
+#endif // CUTILE_C
 
 #endif
