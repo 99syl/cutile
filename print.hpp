b9faf26dbf9ae8fe856cf1082ff6bfe30e4e5337
BaguetteFramboise <120696021+BaguetteFramboise@users.noreply.github.com>
2023-11-20 15:29:48 +0100

    print.h: Refactoring (#9)

---
+++ print.hpp
@@ -0,0 +1,63 @@
+#ifndef CUTILE_PRINT_HPP
+#define CUTILE_PRINT_HPP
+
+#include "./print.h"
+
+template <typename ...Args>
+force_inline void print(allocator* allocator, const char* fmt, Args ...args);
+template <typename ...Args>
+force_inline void print(const char* fmt, Args ...args);
+template <typename ...Args>
+force_inline void print(allocator* allocator, const string* fmt, Args ...args);
+template <typename ...Args>
+force_inline void print(const string* fmt, Args ...args);
+template <typename ...Args> 
+force_inline void println(allocator* allocator, const char* fmt, Args ...args);
+template <typename ...Args> 
+force_inline void println(const char* fmt, Args ...args);
+template <typename ...Args>
+force_inline void println(allocator* allocator, const string* fmt, Args ...args);
+template <typename ...Args>
+force_inline void println(const string* fmt, Args ...args);
+
+
+// Force_Inline implementations:
+template <typename ...Args>
+force_inline void print(allocator* allocator, const char* fmt, Args ...args)
+{
+    string str = format_str(allocator, fmt, args...);
+    print(&str);
+    deallocate(allocator, &str);
+}
+template <typename ...Args>
+force_inline void print(const char* fmt, Args ...args)
+{
+    print(&basic_heap_allocator, fmt, args...);
+}
+
+template <typename ...Args>
+force_inline void print(allocator* allocator, const string* fmt, Args ...args)
+{
+    string str = str_format(allocator, fmt, args...);
+    print(&str);
+    deallocate(allocator, &str);
+}
+template <typename ...Args>
+force_inline void print(const string* fmt, Args ...args)
+{
+    print(&basic_heap_allocator, fmt, args...);
+}
+
+template <typename ...Args> 
+force_inline void println(allocator* allocator, const char* fmt, Args ...args) { print(allocator, fmt, args..., '\n'); }
+
+template <typename ...Args> 
+force_inline void println(const char* fmt, Args ...args) { println(&basic_heap_allocator, fmt, args...); }
+
+template <typename ...Args>
+force_inline void println(allocator* allocator, const string* fmt, Args ...args) { print(allocator, fmt, args..., '\n'); }
+
+template <typename ...Args>
+force_inline void println(const string* fmt, Args ...args) { println(&basic_heap_allocator, fmt, args...); }
+
+#endif // !CUTILE_PRINT_HPP
