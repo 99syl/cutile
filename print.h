64d3206e5b9d01a4ce7942c229df872d004f93b6
Syl <99syl.dev@proton.me>
2023-11-20 15:29:48 +0100

    print.h: Refactoring (#9)

--- print.h
+++ print.h
@@ -6,24 +6,6 @@ void print(const string* str);
 void print_cstr(const char* cstr);
 void println(const string* str);
 void println_cstr(const char* cstr);
-#ifdef CUTILE_CPP
-template <typename ...Args>
-inline void print(allocator* allocator, const char* fmt, Args ...args);
-template <typename ...Args>
-inline void print(const char* fmt, Args ...args);
-template <typename ...Args>
-inline void print(allocator* allocator, const string* fmt, Args ...args);
-template <typename ...Args>
-inline void print(const string* fmt, Args ...args);
-template <typename ...Args> 
-inline void println(allocator* allocator, const char* fmt, Args ...args);
-template <typename ...Args> 
-inline void println(const char* fmt, Args ...args);
-template <typename ...Args>
-inline void println(allocator* allocator, const string* fmt, Args ...args);
-template <typename ...Args>
-inline void println(const string* fmt, Args ...args);
-#endif // CUTILE_CPP
 
 #ifdef CUTILE_IMPLEM
 
@@ -32,6 +14,8 @@ inline void println(const string* fmt, Args ...args);
     #elif defined(__unix__)
         #include <unistd.h>
     #endif
+
+    #include "./str.h"
 
     void print(const string* str)
     {
@@ -71,41 +55,5 @@ inline void println(const string* fmt, Args ...args);
     }
 
 #endif // CUTILE_IMPLEM
-
-// Inline implementations:
-#ifdef CUTILE_CPP
-    template <typename ...Args>
-    inline void print(allocator* allocator, const char* fmt, Args ...args)
-    {
-        string str = format_str(allocator, fmt, args...);
-        print(&str);
-        deallocate(allocator, &str);
-    }
-    template <typename ...Args>
-    inline void print(const char* fmt, Args ...args)
-    {
-        print(&basic_heap_allocator, fmt, args...);
-    }
-    template <typename ...Args>
-    inline void print(allocator* allocator, const string* fmt, Args ...args)
-    {
-        string str = str_format(allocator, fmt, args...);
-        print(&str);
-        deallocate(allocator, &str);
-    }
-    template <typename ...Args>
-    inline void print(const string* fmt, Args ...args)
-    {
-        print(&basic_heap_allocator, fmt, args...);
-    }
-    template <typename ...Args> 
-    inline void println(allocator* allocator, const char* fmt, Args ...args) { print(allocator, fmt, args..., '\n'); }
-    template <typename ...Args> 
-    inline void println(const char* fmt, Args ...args) { println(&basic_heap_allocator, fmt, args...); }
-    template <typename ...Args>
-    inline void println(allocator* allocator, const string* fmt, Args ...args) { print(allocator, fmt, args..., '\n'); }
-    template <typename ...Args>
-    inline void println(const string* fmt, Args ...args) { println(&basic_heap_allocator, fmt, args...); }
-#endif // CUTILE_CPP
 
 #endif // !CUTILE_PRINT_H
