4141892b5709538ad4382b802140885512be8b04
BaguetteFramboise <baguetteframboise@no-reply.com>
2023-12-31 15:19:39 +0100

    test.h: Refactoring

--- test.h
+++ test.h
@@ -5,18 +5,18 @@
 
 extern int test_result;
 
-#define test_assert(predicate)  \
-    {                           \
-        if (!(predicate))       \
-        {                       \
-            println_cstr("Assertion failed: "#predicate);    \
-            test_result = 1;                            \
-        }                                               \
+#define test_assert(predicate)                              \
+    {                                                       \
+        if (!(predicate))                                   \
+        {                                                   \
+            println_cstr("Assertion failed: "#predicate);   \
+            test_result = 1;                                \
+        }                                                   \
     }
 
-#define test_require(predicate) \
-    {   \
-        test_assert(predicate); \
+#define test_require(predicate)             \
+    {                                       \
+        test_assert(predicate);             \
         if (test_result) exit(test_result); \
     }
 
