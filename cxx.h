8709f8cd5f2759ee2888fd95405aa97ee2fdae44
Syl <syl.git@proton.me>
2024-03-07 12:13:35 +0100

    Move sized numeric types to cxx.h

--- cxx.h
+++ cxx.h
@@ -28,6 +28,46 @@
     #define force_inline inline
 #endif
 
+// numeric types:
+
+typedef char            s8;
+typedef unsigned char   u8;
+
+typedef short           s16;
+typedef unsigned short  u16;
+
+////////////// Clang | GCC //////////////
+#if defined(__clang__) || defined(__GNUC__)
+
+typedef __INT32_TYPE__          s32;
+typedef unsigned __INT32_TYPE__ u32;
+
+typedef __INT64_TYPE__          s64;
+typedef unsigned __INT64_TYPE__ u64;
+
+#endif // __clang__ || __GNUC__
+///////////////////////////////////
+
+/////// Microsoft Visual C ////////
+#ifdef _MSC_VER
+
+typedef __int32             s32;
+typedef unsigned __int32    u32;
+
+typedef __int64             s64;
+typedef unsigned __int64    u64;
+
+#endif // _MSC_VER
+///////////////////////////////////
+
+typedef float   f32;
+typedef double  f64;
+
+typedef u8  bool8;
+typedef u8  b8;
+typedef u32 bool32;
+typedef u32 b32;
+
 CUTILE_C_API void exit_process(unsigned int);
 CUTILE_C_API void println_cstr(const char* cstr);
 
