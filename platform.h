fed57faa7610cb09528eb593f484abe48da068ca
Syl <syl.git@proton.me>
2024-03-08 22:31:58 +0100

    platform.h: Add LINUX pp definition

--- platform.h
+++ platform.h
@@ -1,9 +1,17 @@
 #ifndef CUTILE_PLATFORM_H
 #define CUTILE_PLATFORM_H
 
+#define LINUX     0
 #define WINDOWS   0
 #define MAC_OS    0
 #define UNIX_LIKE 0
+
+#ifdef __linux__
+    #undef  LINUX
+    #define LINUX     1
+    #undef  UNIX_LIKE
+    #define UNIX_LIKE 1
+#endif
 
 // INCOMPLETE: What happens on Xbox platforms ?
 #ifdef _WIN32
