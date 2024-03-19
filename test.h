0de885067fc84f22ec2cf93d54ffda09540acef8
Syl <99syl.dev@proton.me>
2024-03-19 16:43:53 +0100

    test.h: More details in assertion fail message + use process_exit instead of exit in test_require

--- test.h
+++ test.h
@@ -1,23 +1,23 @@
 #ifndef CUTILE_TEST_H
 #define CUTILE_TEST_H
 
-#include "./print.h"
+#include "print.h"
 
-extern int test_result;
+CUTILE_C_API int test_result;
 
 #define test_assert(predicate)                              \
     {                                                       \
         if (!(predicate))                                   \
         {                                                   \
-            println_cstr("Assertion failed: "#predicate);   \
+            println_cstr("Assertion failed, " __FILE__ "." current_line_cstr_m ": " #predicate);   \
             test_result = 1;                                \
         }                                                   \
     }
 
-#define test_require(predicate)             \
-    {                                       \
-        test_assert(predicate);             \
-        if (test_result) exit(test_result); \
+#define test_require(predicate)                     \
+    {                                               \
+        test_assert(predicate);                     \
+        if (test_result) process_exit(test_result); \
     }
 
 #ifdef CUTILE_IMPLEM
