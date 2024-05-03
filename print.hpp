4b870944db09d880f31757d0120419a25592b4ac
Syl <99syl.dev@proton.me>
2024-05-03 16:48:17 +0200

    Refactoring

--- print.hpp
+++ print.hpp
@@ -4,64 +4,64 @@
 #include "str_format.hpp"
 
 template <typename ...Args>
-force_inline void print(allocator* allocator, const char* fmt, Args ...args);
+maybe_inline void print(allocator* allocator, const char* fmt, Args ...args);
 template <typename ...Args>
-force_inline void print(const char* fmt, Args ...args);
+maybe_inline void print(const char* fmt, Args ...args);
 template <typename ...Args>
-force_inline void print(allocator* allocator, const string* fmt, Args ...args);
+maybe_inline void print(allocator* allocator, const string* fmt, Args ...args);
 template <typename ...Args>
-force_inline void print(const string* fmt, Args ...args);
+maybe_inline void print(const string* fmt, Args ...args);
 template <typename ...Args> 
-force_inline void println(allocator* allocator, const char* fmt, Args ...args);
+maybe_inline void println(allocator* allocator, const char* fmt, Args ...args);
 template <typename ...Args> 
-force_inline void println(const char* fmt, Args ...args);
+maybe_inline void println(const char* fmt, Args ...args);
 template <typename ...Args>
-force_inline void println(allocator* allocator, const string* fmt, Args ...args);
+maybe_inline void println(allocator* allocator, const string* fmt, Args ...args);
 template <typename ...Args>
-force_inline void println(const string* fmt, Args ...args);
+maybe_inline void println(const string* fmt, Args ...args);
 
 // Implementation:
 template <typename ...Args>
-force_inline void print(allocator* allocator, const char* fmt, Args ...args)
+maybe_inline void print(allocator* allocator, const char* fmt, Args ...args)
 {
     string str = format_str(allocator, fmt, args...);
     print(&str);
     destroy_str(&str);
 }
 template <typename ...Args>
-force_inline void print(const char* fmt, Args ...args)
+maybe_inline void print(const char* fmt, Args ...args)
 {
     print(&default_allocator, fmt, args...);
 }
 
 template <typename ...Args>
-force_inline void print(allocator* allocator, const string* fmt, Args ...args)
+maybe_inline void print(allocator* allocator, const string* fmt, Args ...args)
 {
     string str = str_format(allocator, fmt, args...);
     print(&str);
     destroy_str(&str);
 }
 template <typename ...Args>
-force_inline void print(const string* fmt, Args ...args)
+maybe_inline void print(const string* fmt, Args ...args)
 {
     print(&default_allocator, fmt, args...);
 }
 
 template <typename ...Args> 
-force_inline void println(allocator* allocator, const char* fmt, Args ...args) 
+maybe_inline void println(allocator* allocator, const char* fmt, Args ...args) 
 { 
     string str = format_str(allocator, fmt, args...);
     println(&str);
     destroy_str(&str);
 }
 template <typename ...Args> 
-force_inline void println(const char* fmt, Args ...args) 
+maybe_inline void println(const char* fmt, Args ...args) 
 { 
     println(&default_allocator, fmt, args...); 
 }
 
 template <typename ...Args>
-force_inline void println(allocator* allocator, const string* fmt, Args ...args)
+maybe_inline void println(allocator* allocator, const string* fmt, Args ...args)
 {
     string str = str_format(allocator, fmt, args...);
     println(&str);
@@ -69,7 +69,7 @@ force_inline void println(allocator* allocator, const string* fmt, Args ...args)
 }
 
 template <typename ...Args>
-force_inline void println(const string* fmt, Args ...args) 
+maybe_inline void println(const string* fmt, Args ...args) 
 { 
     println(&default_allocator, fmt, args...); 
 }
