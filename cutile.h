b3098ff87b00eacd82e308e3252b3e1bece104ae
Syl <99syl.dev@proton.me>
2024-05-14 14:14:15 +0200

    Again GCC: Disable changes-meaning warning in place where it's needed.

--- cutile.h
+++ cutile.h
@@ -1,56 +1,60 @@
-#pragma once
+#ifndef CUTILE_H
 
-#ifdef __cplusplus
-    #define CUTILE_CPP
-#elif defined(__STDC_VERSION__)
-    #define CUTILE_C
-#else
-    #error "cutile could not tell if you are using C or C++."
-#endif
-
-#if defined(CUTILE_CPP)
-    #if defined(_MSVC_LANG)
-        #if _MSVC_LANG < 202002L
-            #error "C++ versions prior to C++20 are not supported."
+    #ifdef __cplusplus
+        #define CUTILE_CPP
+    #elif defined(__STDC_VERSION__)
+        #define CUTILE_C
+    #else
+        #error "cutile could not tell if you are using C or C++."
+    #endif
+    
+    #if defined(CUTILE_CPP)
+        #if defined(_MSVC_LANG)
+            #if _MSVC_LANG < 202002L
+                #error "C++ versions prior to C++20 are not supported."
+            #endif
+        #endif
+    #elif defined(CUTILE_C)
+        #if __STDC_VERSION__ < 201112L
+            #error "C versions prior to C11 are not supported."
         #endif
     #endif
-#elif defined(CUTILE_C)
-    #if __STDC_VERSION__ < 201112L
-        #error "C versions prior to C11 are not supported."
-    #endif
-#endif
 
-#ifdef _WIN32
-    #define CUTILE_API_SL_EXPORT __declspec(dllexport)
-#else
-    #define CUTILE_API_SL_EXPORT
-#endif
+    #ifdef _WIN32
+        #define CUTILE_API_SL_EXPORT __declspec(dllexport)
+    #else
+        #define CUTILE_API_SL_EXPORT
+    #endif
+    
+    #ifdef _WIN32
+        #define CUTILE_API_IMPORT_SL __declspec(dllimport)
+    #else
+        #define CUTILE_API_IMPORT_SL
+    #endif
 
-#ifdef _WIN32
-    #define CUTILE_API_IMPORT_SL __declspec(dllimport)
-#else
-    #define CUTILE_API_IMPORT_SL
-#endif
+    #if defined(COMPILE_CUTILE_DLL)
+        #ifdef CUTILE_CPP
+            #define CUTILE_C_API extern "C" CUTILE_API_SL_EXPORT
+            #define CUTILE_CPP_API extern CUTILE_API_SL_EXPORT
+        #else
+            #define CUTILE_C_API extern CUTILE_API_SL_EXPORT
+        #endif
+    #elif defined(IMPORT_CUTILE_DLL)
+        #ifdef CUTILE_CPP
+            #define CUTILE_C_API extern "C" CUTILE_API_IMPORT_SL
+            #define CUTILE_CPP_API extern CUTILE_API_IMPORT_SL
+        #else
+            #define CUTILE_C_API extern CUTILE_API_IMPORT_SL
+        #endif
+    #else
+        #ifdef CUTILE_CPP
+            #define CUTILE_C_API extern "C"
+            #define CUTILE_CPP_API extern
+        #else
+            #define CUTILE_C_API extern
+        #endif
+    #endif
 
-#if defined(COMPILE_CUTILE_DLL)
-    #ifdef CUTILE_CPP
-        #define CUTILE_C_API extern "C" CUTILE_API_SL_EXPORT
-        #define CUTILE_CPP_API extern CUTILE_API_SL_EXPORT
-    #else
-        #define CUTILE_C_API extern CUTILE_API_SL_EXPORT
-    #endif
-#elif defined(IMPORT_CUTILE_DLL)
-    #ifdef CUTILE_CPP
-        #define CUTILE_C_API extern "C" CUTILE_API_IMPORT_SL
-        #define CUTILE_CPP_API extern CUTILE_API_IMPORT_SL
-    #else
-        #define CUTILE_C_API extern CUTILE_API_IMPORT_SL
-    #endif
-#else
-    #ifdef CUTILE_CPP
-        #define CUTILE_C_API extern "C"
-        #define CUTILE_CPP_API extern
-    #else
-        #define CUTILE_C_API extern
-    #endif
+    #define CUTILE_H
+
 #endif
--- hash_table.h
+++ hash_table.h
@@ -1,208 +1,217 @@
 #ifndef CUTILE_HASH_TABLE_H
 
-#include "memory.h"
+    #include "memory.h"
 
-typedef struct hash_table_entry hash_table_entry;
-typedef struct hash_table_entry
-{
-    u64               key_length;
-    u8*               key;
-    u8*               value;
-    hash_table_entry* next;
-} hash_table_entry;
+    typedef struct hash_table_entry hash_table_entry;
+    typedef struct hash_table_entry
+    {
+        u64               key_length;
+        u8*               key;
+        u8*               value;
+        hash_table_entry* next;
+    } hash_table_entry;
 
-typedef u64 (*hash_function_ptr)(u8* data, u64 data_size);
+    typedef u64 (*hash_function_ptr)(u8* data, u64 data_size);
 
-typedef b8  (*compare_keys_function_ptr)(u8* lhs, u64 lhs_size, u8* rhs, u64 rhs_size);
+    typedef b8  (*compare_keys_function_ptr)(u8* lhs, u64 lhs_size, u8* rhs, u64 rhs_size);
 
-CUTILE_C_API b8 default_compare_keys(u8* lhs, u64 lhs_size, u8* rhs, u64 rhs_size);
+    CUTILE_C_API b8 default_compare_keys(u8* lhs, u64 lhs_size, u8* rhs, u64 rhs_size);
 
-typedef struct hash_table
-{
-    hash_table_entry** entries;
+    #if defined(__clang__) || defined(__GNUC__)
+        #pragma GCC diagnostic ignored "-Wchanges-meaning"
+    #endif
     
-    u32 count;
-    u32 size;
-
-    hash_function_ptr          hash_function;
-    compare_keys_function_ptr  compare_keys_function;
-    u64                        value_size_in_bytes;
-
-    allocator* allocator;
-} hash_table;
-
-// This call might fail in case allocation fails.
-// To check for allocation failure just check that hash_table.entries pointer is null.
-CUTILE_C_API hash_table create_hash_table(u32                       base_size,
-                                          hash_function_ptr         hash_function,
-                                          compare_keys_function_ptr compare_keys_function,
-                                          u64                       value_size_in_bytes,
-                                          allocator*                allocator);
-
-CUTILE_C_API void       destroy_hash_table(hash_table* hash_table);
-
-// Call might fail if allocation fails, return nullptr in that case.
-// Returns a pointer to the value stored in the inserted hash_table_entry otherwise.
-// If the key is already emplaced, this call will not change the state of the given hash_table.
-// The key content is copied so you can do whatever you want with it after this call.
-CUTILE_C_API u8*        hash_table_emplace(hash_table* hash_table, u8* key, u64 key_length);
-
-// Returns false if the entry could not be removed because the key has not been found.
-// Returns true if the entry has been successfully removed.
-CUTILE_C_API b8         hash_table_remove(hash_table* hash_table, u8* key, u64 key_length);
-
-// Returns a pointer to the value associated to the given key.
-// Returns nullptr if the key has not been found.
-CUTILE_C_API u8*        hash_table_get(hash_table* hash_table, u8* key, u64 key_length);
-
-CUTILE_C_API void       hash_table_for_each_entry(hash_table* hash_table,
-                                                  void (*)(hash_table_entry* entry));
-
-#define                 hash_table_for_each_entry_m(hash_table, ...)
-
-#ifdef CUTILE_IMPLEM
-
-    hash_table create_hash_table(u32                       base_size,
-                                 hash_function_ptr         hash_function,
-                                 compare_keys_function_ptr compare_keys_function,
-                                 u64                       value_size_in_bytes,
-                                 allocator*                allocator)
+    typedef struct hash_table
     {
-        hash_table res;
+        hash_table_entry** entries;
+        
+        u32 count;
+        u32 size;
+    
+        hash_function_ptr          hash_function;
+        compare_keys_function_ptr  compare_keys_function;
+        u64                        value_size_in_bytes;
+    
+        allocator* allocator;
+    } hash_table;
+    
+    
+    #if defined(__clang__) || defined(__GNUC__)
+        #pragma GCC diagnostic pop
+    #endif
+    
+    // This call might fail in case allocation fails.
+    // To check for allocation failure just check that hash_table.entries pointer is null.
+    CUTILE_C_API hash_table create_hash_table(u32                       base_size,
+                                              hash_function_ptr         hash_function,
+                                              compare_keys_function_ptr compare_keys_function,
+                                              u64                       value_size_in_bytes,
+                                              allocator*                allocator);
 
-        res.entries = allocate_many_m(allocator, hash_table_entry*, base_size);
-        if (res.entries == nullptr) return res;
+    CUTILE_C_API void       destroy_hash_table(hash_table* hash_table);
 
-        for (u32 i = 0; i < base_size; ++i)
+    // Call might fail if allocation fails, return nullptr in that case.
+    // Returns a pointer to the value stored in the inserted hash_table_entry otherwise.
+    // If the key is already emplaced, this call will not change the state of the given hash_table.
+    // The key content is copied so you can do whatever you want with it after this call.
+    CUTILE_C_API u8*        hash_table_emplace(hash_table* hash_table, u8* key, u64 key_length);
+
+    // Returns false if the entry could not be removed because the key has not been found.
+    // Returns true if the entry has been successfully removed.
+    CUTILE_C_API b8         hash_table_remove(hash_table* hash_table, u8* key, u64 key_length);
+
+    // Returns a pointer to the value associated to the given key.
+    // Returns nullptr if the key has not been found.
+    CUTILE_C_API u8*        hash_table_get(hash_table* hash_table, u8* key, u64 key_length);
+
+    CUTILE_C_API void       hash_table_for_each_entry(hash_table* hash_table,
+                                                      void (*)(hash_table_entry* entry));
+
+    #define                 hash_table_for_each_entry_m(hash_table, ...)
+
+    #ifdef CUTILE_IMPLEM
+
+        hash_table create_hash_table(u32                       base_size,
+                                     hash_function_ptr         hash_function,
+                                     compare_keys_function_ptr compare_keys_function,
+                                     u64                       value_size_in_bytes,
+                                     allocator*                allocator)
         {
-            res.entries[i] = nullptr;
-        }
-
-        res.count = 0;
-        res.size = base_size;
-        res.hash_function = hash_function;
-        res.compare_keys_function = compare_keys_function;
-        res.value_size_in_bytes = value_size_in_bytes;
-        res.allocator = allocator;
-        return res;
-    }
-
-    void destroy_hash_table(hash_table* hash_table)
-    {
-        for (u32 i = 0; i < hash_table->size; ++i)
-        {
-            hash_table_entry* head = hash_table->entries[i];
-            while (head != nullptr)
+            hash_table res;
+    
+            res.entries = allocate_many_m(allocator, hash_table_entry*, base_size);
+            if (res.entries == nullptr) return res;
+    
+            for (u32 i = 0; i < base_size; ++i)
             {
-                hash_table_entry* tmp = head;
-                head = head->next;
-                deallocate(hash_table->allocator, tmp);
+                res.entries[i] = nullptr;
+            }
+    
+            res.count = 0;
+            res.size = base_size;
+            res.hash_function = hash_function;
+            res.compare_keys_function = compare_keys_function;
+            res.value_size_in_bytes = value_size_in_bytes;
+            res.allocator = allocator;
+            return res;
+        }
+    
+        void destroy_hash_table(hash_table* hash_table)
+        {
+            for (u32 i = 0; i < hash_table->size; ++i)
+            {
+                hash_table_entry* head = hash_table->entries[i];
+                while (head != nullptr)
+                {
+                    hash_table_entry* tmp = head;
+                    head = head->next;
+                    deallocate(hash_table->allocator, tmp);
+                }
+            }
+    
+            deallocate(hash_table->allocator, hash_table->entries);
+        }
+    
+        u8* hash_table_emplace(hash_table* hash_table, u8* key, u64 key_length)
+        {
+            u64 hash = hash_table->hash_function(key, key_length);
+            u64 index = (hash % hash_table->size) - 1;
+    
+            hash_table_entry** head = &hash_table->entries[index];
+            while (*head != nullptr)
+            {
+                hash_table_entry* entry = *head;
+                if (hash_table->compare_keys_function(key, key_length, entry->key, entry->key_length))
+                {
+                    return entry->value;
+                }
+                head = &entry->next;
+            }
+    
+            hash_table_entry* new_entry = (hash_table_entry*)allocate(hash_table->allocator, sizeof(hash_table_entry) + hash_table->value_size_in_bytes + key_length);
+            if (new_entry == nullptr) return nullptr;
+            new_entry->key_length = key_length;
+            new_entry->key = ((u8*)new_entry) + sizeof(hash_table_entry) + hash_table->value_size_in_bytes;
+            copy_u8_memory(new_entry->key, key, key_length);
+            new_entry->value = (u8*)new_entry + sizeof(hash_table_entry);
+            new_entry->next = nullptr;
+            *head = new_entry;
+            hash_table->count++;
+    
+            return new_entry->value;
+        }
+    
+        b8 hash_table_remove(hash_table* hash_table, u8* key, u64 key_length)
+        {
+            u64 hash = hash_table->hash_function(key, key_length);
+            u64 index = (hash % hash_table->size) - 1;
+    
+            hash_table_entry** head = &hash_table->entries[index];
+    
+            while (*head != nullptr)
+            {
+                hash_table_entry* entry = *head;
+                if (hash_table->compare_keys_function(key, key_length, entry->key, entry->key_length))
+                {
+                    hash_table_entry* next = entry->next;
+                    deallocate(hash_table->allocator, entry);
+                    *head = next;
+                    hash_table->count--;
+                    return b8_true;
+                }
+                head = &entry->next;
+            }
+            return b8_false;
+        }
+    
+        u8* hash_table_get(hash_table* hash_table, u8* key, u64 key_length)
+        {
+            u64 hash = hash_table->hash_function(key, key_length);
+            u64 index = (hash % hash_table->size) - 1;
+    
+            hash_table_entry** head = &hash_table->entries[index];
+    
+            while (*head != nullptr)
+            {
+                hash_table_entry* entry = *head;
+                if (hash_table->compare_keys_function(key, key_length, entry->key, entry->key_length))
+                {
+                    return entry->value;
+                }
+                head = &entry->next;
+            }
+            return nullptr;
+        }
+    
+        void hash_table_for_each_entry(hash_table* hash_table,
+                                       void (*call)(hash_table_entry* entry))
+        {
+            for (u32 i = 0; i < hash_table->size; ++i)
+            {
+                hash_table_entry* head = hash_table->entries[i];
+                while (head != nullptr)
+                {
+                    call(head);
+                    head = head->next;
+                }
             }
         }
-
-        deallocate(hash_table->allocator, hash_table->entries);
-    }
-
-    u8* hash_table_emplace(hash_table* hash_table, u8* key, u64 key_length)
-    {
-        u64 hash = hash_table->hash_function(key, key_length);
-        u64 index = (hash % hash_table->size) - 1;
-
-        hash_table_entry** head = &hash_table->entries[index];
-        while (*head != nullptr)
-        {
-            hash_table_entry* entry = *head;
-            if (hash_table->compare_keys_function(key, key_length, entry->key, entry->key_length))
-            {
-                return entry->value;
-            }
-            head = &entry->next;
+    
+    #endif // CUTILE_IMPLEM
+    
+    #undef                  hash_table_for_each_entry_m
+    #define                 hash_table_for_each_entry_m(ht, ...)            \
+        {                                                                   \
+            for (u32 i = 0; i < (ht)->size; ++i)                            \
+            {                                                               \
+                hash_table_entry* it = (ht)->entries[i];                    \
+                while (it != nullptr)                                       \
+                {                                                           \
+                    __VA_ARGS__;                                            \
+                    it = it->next;                                          \
+                }                                                           \
+            }                                                               \
         }
-
-        hash_table_entry* new_entry = (hash_table_entry*)allocate(hash_table->allocator, sizeof(hash_table_entry) + hash_table->value_size_in_bytes + key_length);
-        if (new_entry == nullptr) return nullptr;
-        new_entry->key_length = key_length;
-        new_entry->key = ((u8*)new_entry) + sizeof(hash_table_entry) + hash_table->value_size_in_bytes;
-        copy_u8_memory(new_entry->key, key, key_length);
-        new_entry->value = (u8*)new_entry + sizeof(hash_table_entry);
-        new_entry->next = nullptr;
-        *head = new_entry;
-        hash_table->count++;
-
-        return new_entry->value;
-    }
-
-    b8 hash_table_remove(hash_table* hash_table, u8* key, u64 key_length)
-    {
-        u64 hash = hash_table->hash_function(key, key_length);
-        u64 index = (hash % hash_table->size) - 1;
-
-        hash_table_entry** head = &hash_table->entries[index];
-
-        while (*head != nullptr)
-        {
-            hash_table_entry* entry = *head;
-            if (hash_table->compare_keys_function(key, key_length, entry->key, entry->key_length))
-            {
-                hash_table_entry* next = entry->next;
-                deallocate(hash_table->allocator, entry);
-                *head = next;
-                hash_table->count--;
-                return b8_true;
-            }
-            head = &entry->next;
-        }
-        return b8_false;
-    }
-
-    u8* hash_table_get(hash_table* hash_table, u8* key, u64 key_length)
-    {
-        u64 hash = hash_table->hash_function(key, key_length);
-        u64 index = (hash % hash_table->size) - 1;
-
-        hash_table_entry** head = &hash_table->entries[index];
-
-        while (*head != nullptr)
-        {
-            hash_table_entry* entry = *head;
-            if (hash_table->compare_keys_function(key, key_length, entry->key, entry->key_length))
-            {
-                return entry->value;
-            }
-            head = &entry->next;
-        }
-        return nullptr;
-    }
-
-    void hash_table_for_each_entry(hash_table* hash_table,
-                                   void (*call)(hash_table_entry* entry))
-    {
-        for (u32 i = 0; i < hash_table->size; ++i)
-        {
-            hash_table_entry* head = hash_table->entries[i];
-            while (head != nullptr)
-            {
-                call(head);
-                head = head->next;
-            }
-        }
-    }
-
-#endif // CUTILE_IMPLEM
-
-#undef                  hash_table_for_each_entry_m
-#define                 hash_table_for_each_entry_m(ht, ...)            \
-    {                                                                   \
-        for (u32 i = 0; i < (ht)->size; ++i)                            \
-        {                                                               \
-            hash_table_entry* it = (ht)->entries[i];                    \
-            while (it != nullptr)                                       \
-            {                                                           \
-                __VA_ARGS__;                                            \
-                it = it->next;                                          \
-            }                                                           \
-        }                                                               \
-    }
-
-#define CUTILE_HASH_TABLE_H
+    
+    #define CUTILE_HASH_TABLE_H
 #endif
