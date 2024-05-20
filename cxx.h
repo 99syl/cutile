7cf2b5f8ba0ceeaa041034e9624ebbae25571292
Syl <99syl.dev@proton.me>
2024-05-20 10:36:04 +0200

    Use standard include guards in cxx.h

--- cxx.h
+++ cxx.h
@@ -1,97 +1,95 @@
-#pragma once
+#ifndef CUTILE_CXX_H
 
-#include "cutile.h"
+    #include "cutile.h"
 
-#define internal static
-#define persist static
+    #define internal static
+    #define persist static
 
-// CUTILE_STATIC_ASSERT
-#ifdef CUTILE_CPP
-    #define CUTILE_STATIC_ASSERT(a) static_assert(a)
-#else // CUTILE_C
-    #if __STDC_VERSION__ >= 202311L // C23
+    // CUTILE_STATIC_ASSERT
+    #ifdef CUTILE_CPP
         #define CUTILE_STATIC_ASSERT(a) static_assert(a)
-    #elif __STDC_VERSION__ >= 201112L // C11
-        #define CUTILE_STATIC_ASSERT(a) _Static_assert(a)
-    #endif // __STDC_VERSION__
-#endif // CUTILE_CPP
+    #else // CUTILE_C
+        #if __STDC_VERSION__ >= 202311L // C23
+            #define CUTILE_STATIC_ASSERT(a) static_assert(a)
+        #elif __STDC_VERSION__ >= 201112L // C11
+            #define CUTILE_STATIC_ASSERT(a) _Static_assert(a)
+        #endif // __STDC_VERSION__
+    #endif // CUTILE_CPP
 
-#define maybe_inline inline
+    #define maybe_inline inline
 
-// force_inline
-#ifdef _MSC_VER
-    #define force_inline __forceinline
-#else
-    #define force_inline inline
+    // numeric types:
+    ///////////////////////////////////////////////////////
+    typedef char            s8;
+    typedef unsigned char   u8;
+
+    typedef short           s16;
+    typedef unsigned short  u16;
+    ///////////////////////////////////////////////////////
+
+    ////////////// Clang | GCC //////////////
+    #if defined(__clang__) || defined(__GNUC__)
+
+    typedef __INT32_TYPE__          s32;
+    typedef unsigned __INT32_TYPE__ u32;
+
+    typedef __INT64_TYPE__          s64;
+    typedef unsigned __INT64_TYPE__ u64;
+
+    #endif // __clang__ || __GNUC__
+    ///////////////////////////////////
+
+    /////// Microsoft Visual C ////////
+    #ifdef _MSC_VER
+
+    typedef __int32             s32;
+    typedef unsigned __int32    u32;
+
+    typedef __int64             s64;
+    typedef unsigned __int64    u64;
+
+    #endif // _MSC_VER
+    ///////////////////////////////////
+
+    typedef float   f32;
+    typedef double  f64;
+
+    typedef u8  bool8;
+    typedef u8  b8;
+    typedef u32 bool32;
+    typedef u32 b32;
+    ///////////////////////////////////////////////////////
+
+    // CUTILE_ASSERT
+    #ifdef CUTILE_ENABLE_ASSERT
+        CUTILE_C_API void exit_process(unsigned int);
+        CUTILE_C_API void println_cstr(const char* cstr);
+        #define CUTILE_ASSERT(pred, ...)                                    \
+        {                                                                   \
+            if (!(pred))                                                    \
+            {                                                               \
+                println_cstr("Assertion Failed: " #pred ". " __VA_ARGS__);  \
+                exit_process(1);                                            \
+            }                                                               \
+        }
+    #else
+        #define CUTILE_ASSERT(pred, ...)
+    #endif
+
+    // nullptr
+    #ifdef CUTILE_C
+        #define nullptr 0
+    #endif
+
+    // field_offset
+    // Gets the offset (in bytes) of a field inside a structure.
+    #define field_offset(type, member) ((u64)(&((type*)0x0)->member))
+
+    #define stringify_m(x) stringify2_m(x)
+    #define stringify2_m(x) #x
+
+    #define current_line_cstr_m stringify_m(__LINE__)
+
+    #define CUTILE_CXX_H
+
 #endif
-
-// numeric types:
-///////////////////////////////////////////////////////
-typedef char            s8;
-typedef unsigned char   u8;
-
-typedef short           s16;
-typedef unsigned short  u16;
-
-////////////// Clang | GCC //////////////
-#if defined(__clang__) || defined(__GNUC__)
-
-typedef __INT32_TYPE__          s32;
-typedef unsigned __INT32_TYPE__ u32;
-
-typedef __INT64_TYPE__          s64;
-typedef unsigned __INT64_TYPE__ u64;
-
-#endif // __clang__ || __GNUC__
-///////////////////////////////////
-
-/////// Microsoft Visual C ////////
-#ifdef _MSC_VER
-
-typedef __int32             s32;
-typedef unsigned __int32    u32;
-
-typedef __int64             s64;
-typedef unsigned __int64    u64;
-
-#endif // _MSC_VER
-///////////////////////////////////
-
-typedef float   f32;
-typedef double  f64;
-
-typedef u8  bool8;
-typedef u8  b8;
-typedef u32 bool32;
-typedef u32 b32;
-///////////////////////////////////////////////////////
-
-CUTILE_C_API void exit_process(unsigned int);
-CUTILE_C_API void println_cstr(const char* cstr);
-
-// CUTILE_ASSERT
-#ifdef CUTILE_ENABLE_ASSERT
-    #define CUTILE_ASSERT(pred, ...)                                        \
-    {                                                                   \
-        if (!(pred))                                                    \
-        {                                                               \
-            println_cstr("Assertion Failed: " #pred ". " __VA_ARGS__);  \
-            exit_process(1);                                            \
-        }                                                               \
-    }
-#else
-    #define CUTILE_ASSERT(pred, ...)
-#endif
-
-// nullptr
-#ifdef CUTILE_C
-    #define nullptr 0
-#endif
-
-// field_offset
-#define field_offset(type, member) ((u64)(&((type*)0x0)->member))
-
-#define stringify_m(x) stringify2_m(x)
-#define stringify2_m(x) #x
-
-#define current_line_cstr_m stringify_m(__LINE__)
