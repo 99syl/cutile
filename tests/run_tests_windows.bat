8616545c9dd60b973367858f4340e10eab9134e0
Syl <99syl.dev@proton.me>
2024-05-26 10:08:17 +0200

    tests:
    - Fix str tests
    - Remove endian.c
    - Add a batch script to quickly compile and run tests on Windows MSVC

---
+++ tests/run_tests_windows.bat
@@ -0,0 +1,10 @@
+@echo off
+
+SET work_dir=%~dp0
+
+SET c_cflags=/std:c11 /Z7
+
+SET tests=(array base64 blowfish cxx filesystem hash_table ini shared_library str)
+
+for %%i in %tests% do cl /Z7 /std:c11 /utf-8 %work_dir%\%%i.c /link /OUT:%work_dir%\%%i.exe
+for %%i in %tests% do %%i.exe
