6fbd71e18bffaf0a07520d7056ae7eeed9ed872c
Syl <99syl.dev@proton.me>
2024-05-30 08:49:55 +0200

    endian.h: MSVC compiler warning when using functions to check endianness.
    MSVC was saying that result was uninitialized, even though they should using the endianness macros... Maybe the macros are not working properly ? I should investigate that later.

--- endian.h
+++ endian.h
@@ -2,21 +2,20 @@
 
     #include "cxx.h"
 
+    // TODO: Are those macros working properly ?
     #define cutile_is_cpu_big_endian_m(out)
     #define cutile_is_cpu_little_endian_m(out)
 
     maybe_inline b8 cutile_is_cpu_big_endian()
     {
-        b8 res;
-        cutile_is_cpu_big_endian_m(res);
-        return res;
+        s32 __cu_is_be_i = 1;
+        return *((s8*)&__cu_is_be_i) == 0;
     }
 
     maybe_inline b8 cutile_is_cpu_little_endian()
     {
-        b8 res;
-        cutile_is_cpu_little_endian_m(res);
-        return res;
+        s32 __cu_is_be_i = 1;
+        return *((s8*)&__cu_is_be_i) == 1;
     }
 
     ////////////////////////////////////////
