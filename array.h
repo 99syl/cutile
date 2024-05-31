e6003793918a2ba40a7b5d26c490870f09bba151
Syl <99syl.dev@proton.me>
2024-05-31 23:48:19 +0200

    array.h: Add array view types.

--- array.h
+++ array.h
@@ -1,15 +1,26 @@
 #ifndef CUTILE_ARRAY_H
-
     #include "memory.h"
 
     #define cutile_fixed_array_length_m(arr)  (sizeof(arr)/sizeof(arr[0]))
+
+    #define cutile_array_view(type) type##_array_view
+
+    #define cutile_generate_array_view_m(type) cutile_generate_named_array_view_m(type, type##_array_view)
+    #define cutile_generate_named_array_view_m(type, name)  \
+        typedef struct                                      \
+        {                                                   \
+            const type* data;                               \
+            u32   count;                                    \
+        } name;                                             \
 
     #define cutile_generate_array_m(type) cutile_generate_named_array_m(type, type##_array)
 
     #define cutile_array(type) type##_array
     
     #ifndef CUTILE_API_NO_SHORT_NAMES
-        #define array(type) type##_array
+        #define fixed_array_length_m(arr) cutile_fixed_array_length_m(arr)
+        #define array_view(type) cutile_array_view(type)
+        #define array(type) cutile_array(type)
         #define cutile_generate_named_array_m(type, name)               \
             cutile_generate_named_array_interface_m(type, name)         \
             cutile_generate_named_array_short_interface_m(type, name)   \
@@ -192,6 +203,14 @@
         maybe_inline void cutile_reverse_##name(name* array) { cutile_reverse_##name##_slice(array, 0, array->count); }                             \
 
     // Generate some basic array types.
+    cutile_generate_array_view_m(s8);
+    cutile_generate_array_view_m(u8);
+    cutile_generate_array_view_m(s16);
+    cutile_generate_array_view_m(u16);
+    cutile_generate_array_view_m(s32);
+    cutile_generate_array_view_m(u32);
+    cutile_generate_array_view_m(s64);
+    cutile_generate_array_view_m(u64);
     cutile_generate_array_m(s8);
     cutile_generate_array_m(u8);
     cutile_generate_array_m(s16);
