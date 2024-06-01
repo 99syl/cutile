c1502290dc44922d438b0fe5672e9d67ba0147e0
Syl <99syl.dev@proton.me>
2024-06-01 14:05:22 +0200

    filesystem: Fix cutile_concat_file_paths.
    The implementation had the wrong symbol name so cutile_concat_file_paths was never compiled into binary.
    
    Also the implementation (with the wrong name) had a bug when calculating C-strings length.

--- tests/filesystem.c
+++ tests/filesystem.c
@@ -17,10 +17,16 @@ int main()
     cutile_string dummy_relative_path = cutile_concat_file_paths_str(&test_dir_name, &dummy_file_name, allocator);
     cutile_string dummy_absolute_path = cutile_concat_file_paths_str(&program_dir_path, &dummy_relative_path, allocator);
 
+    const char* p1 = "foo/bar/t";
+    const char* p2 = "u/vv";
+    const char* p3 = cutile_concat_file_paths(p1, p2, allocator);
+
     #if WINDOWS
         cutile_test_assert_m(cutile_str_equals_cstr(&dummy_relative_path, "filesystem_test_dir\\dummy_file.txt"));
+        cutile_test_assert_m(cutile_cstr_equals(p3, "foo/bar/t\\u/vv"));
     #elif UNIX_LIKE
         cutile_test_assert_m(cutile_str_equals_cstr(&dummy_relative_path, "filesystem_test_dir/dummy_file.txt"));
+        cutile_test_assert_m(cutile_cstr_equals(p3, "foo/bar/t/u/vv"));
     #endif
     cutile_test_assert_m(cutile_file_exists_str(&dummy_absolute_path, allocator));
 
