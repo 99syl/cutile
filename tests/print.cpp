bc05c5a7e73da93dfae7079513df0b452ad80f35
Syl <99syl.dev@proton.me>
2024-06-06 15:45:07 +0200

    Implement network interface for Linux.
    Change test name socket to network
    
    Remove print.cpp test

--- tests/print.cpp
+++
@@ -1,9 +0,0 @@
-#define CUTILE_IMPLEM
-#include "../print.hpp"
-
-allocator default_allocator = create_default_heap_allocator();
-
-int main(int ac, char** av)
-{
-    println("Test %", (char)3);
-}
